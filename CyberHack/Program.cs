5580e8d30ee8af26f68563c9114013193557800a
Francisco Caldeira <kikocaldeira2007@gmail.com>
2026-03-05 17:23:34 +0000

    Fifth Commit

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -28,39 +28,8 @@ namespace CyberHack
                 break;
             }
             string Tools=Console.ReadLine();
-            string T1="";
-            string T2="";
-            string T3="";
-            string Aux="";
-            foreach(char letter in Tools)
-            {
-                
-                if (char.IsWhiteSpace(letter))
-                {
-                    if (T1=="" && T2=="" && T3=="")
-                    {
-                        T1=Aux;
-                        Aux="";
-                    }
-                    else if (T2 == "" && T1!="" && T3=="")
-                    {
-                        T2=Aux;
-                        Aux="";
-                    }
-                    else if (T3 == "" && T2!="" && T1!="")
-                    {
-                        T3=Aux;
-                        Aux="";
-                    }
-                }
-                else
-                {
-                    Aux+=letter;
-                }
-            }
-            Console.WriteLine(T1);
-            Console.WriteLine(T2);
-            Console.WriteLine(T3);
+            string[] Tools2=Tools.Split(' ');
+
         }
     }
 }
